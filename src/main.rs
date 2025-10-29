ea172fcb5b1d9c168404f69da4ba4fe2a8f9cce9
FlamingoLindo <vitorantunes2003@gmail.com>
2025-10-28 23:47:26 -0300

    feat: start

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello word!")
+}
+
+#[post("/echo")]
+async fn echo(body: String) -> impl  Responder{
+    HttpResponse::Ok().body(body)
+}
+
+async fn manual_hello() -> impl  Responder {
+    HttpResponse::Ok().body("Yo!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
