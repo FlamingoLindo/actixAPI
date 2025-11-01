47340af7cbaf208972ea3962baef203e9c5ceb53
FlamingoLindo <vitorantunes2003@gmail.com>
2025-11-01 18:27:00 -0300

    feat: user services

--- src/routes/config.rs
+++ src/routes/config.rs
@@ -1,4 +1,14 @@
 use actix_web::web;
 
-pub fn config(_conf: &mut web::ServiceConfig) {
+use super::user::{create_user, delete_user, get_user, get_users, update_user};
+
+pub fn config(conf: &mut web::ServiceConfig) {
+    let scope = web::scope("/api/users")
+        .service(get_users)
+        .service(get_user)
+        .service(create_user)
+        .service(update_user)
+        .service(delete_user);
+
+    conf.service(scope);
 }
