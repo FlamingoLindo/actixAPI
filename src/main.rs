fee742b0ac24f2cb6c513f647ea457fe27417cd1
FlamingoLindo <vitorantunes2003@gmail.com>
2025-11-16 14:23:54 -0300

    feat: add Svelte URL to CORS

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000")
+            .allowed_origin("http://localhost:5173")
             .allowed_methods(vec!["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
             .allowed_headers(vec![
                 header::CONTENT_TYPE,
